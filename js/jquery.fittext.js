cb639cb7f8d70a46cb46746d1b507198b56cf068
buddhika <buddhikad@projectxspace.com>
2023-06-24 10:31:35 +0100

    Added http to https

--- js/jquery.fittext.js
+++ js/jquery.fittext.js
@@ -2,9 +2,9 @@
 /*!
 * FitText.js 1.1
 *
-* Copyright 2011, Dave Rupert http://daverupert.com
+* Copyright 2011, Dave Rupert https://daverupert.com
 * Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
+* https://sam.zoy.org/wtfpl/
 *
 * Date: Thu May 05 14:23:00 2011 -0600
 */
